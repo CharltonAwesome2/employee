9efa994ec2830b82e0f5e3387720de1847dcafd3
CharltonAwesome1 <CharltonAwesome@gmail.com>
2026-02-10 11:11:37 +0200

    Complete User class. Add getters and setters.

--- src/main/java/za/ac/cput/domain/user.java
+++ src/main/java/za/ac/cput/domain/user.java
@@ -1,6 +1,33 @@
 package za.ac.cput.domain;
 
 public class user {
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
     private String id;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     private String name;
+
+    public String getNationalId() {
+        return nationalId;
+    }
+
+    public void setNationalId(String nationalId) {
+        this.nationalId = nationalId;
+    }
+
+    private String nationalId;
 }
